3114ad38f861ea33a5f7a42582da5e6355ea7d1d
Tanvi Sonar <tanvisonar2611@gmail.com>
2026-05-22 23:18:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,72 @@
-// TIMER
+// THEME TOGGLE
+const toggleBtn = document.getElementById("themeToggle");
 
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  document.body.classList.toggle("light");
+
+  if(document.body.classList.contains("dark")){
+    toggleBtn.innerText = "☀️";
+  } else {
+    toggleBtn.innerText = "🌙";
+  }
+});
+
+// DEFAULT THEME
+document.body.classList.add("dark");
+
+
+// TIMER LOGIC
 let time = 1500;
 let timer;
 
-function updateTimer() {
-  let minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  document.getElementById("timer").innerText =
-    `${minutes}:${seconds}`;
+function updateTimer(){
+  let min = Math.floor(time / 60);
+  let sec = time % 60;
+  sec = sec < 10 ? "0"+sec : sec;
+  document.getElementById("timer").innerText = `${min}:${sec}`;
 }
 
-function startTimer() {
+function startTimer(){
   timer = setInterval(() => {
-    if (time > 0) {
+    if(time > 0){
       time--;
       updateTimer();
     }
-  }, 1000);
+  },1000);
 }
 
-function pauseTimer() {
+function pauseTimer(){
   clearInterval(timer);
 }
 
-function resetTimer() {
+function resetTimer(){
   clearInterval(timer);
   time = 1500;
   updateTimer();
 }
 
-updateTimer();
+// CUSTOM TIMER
+function setTimer(){
+  let input = document.getElementById("minutesInput").value;
+  if(input === "" || input <= 0) return;
+
+  time = input * 60;
+  updateTimer();
+}
 
 
 // TASKS
-
-function addTask() {
+function addTask(){
   let input = document.getElementById("taskInput");
-  let taskText = input.value;
-
-  if (taskText === "") return;
+  if(input.value === "") return;
 
   let li = document.createElement("li");
-
-  li.innerHTML = `
-    ${taskText}
-    <button onclick="this.parentElement.remove()">X</button>
-  `;
+  li.innerHTML = `${input.value} <button onclick="this.parentElement.remove()">X</button>`;
 
   document.getElementById("taskList").appendChild(li);
 
   input.value = "";
 }
+
+updateTimer();
